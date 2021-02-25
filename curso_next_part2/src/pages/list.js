9327a78ebd0cc1993ec4d78c0714c9d66e856cce
matias-ibserveis <matias@ibserveis.com>
2021-02-25 21:02:36 +0100

    list users OK

--- curso_next_part2/src/pages/list.js
+++ curso_next_part2/src/pages/list.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
-export default function List({ownersList}) {
+export default function List({usuariosList}) {
   
   return (
     <div>
-      {ownersList.map((element, index) => (
+      {usuariosList.map((element, index) => (
         <div key={index}>
-          <Link as={`/${element.namme}/${element.email}`} href="/[vehicle]/[person]">
+          <Link as={`${element.name}`} href={`/usuario?id=${element.id}`}>
             <a>
-              Navigate to {element.name}'s {element.email}
+              Ver la ficha de {element.id} - {element.name}
             </a>
           </Link>
         </div>
@@ -21,6 +21,6 @@ export default function List({ownersList}) {
 List.getInitialProps = async () => {
   //const response = await fetch('http://localhost:3000/vehicles');
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const ownersList = await response.json();
-  return {ownersList: ownersList}
+  const usuariosList = await response.json();
+  return {usuariosList: usuariosList}
 }
