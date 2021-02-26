4c05d989a2b3f954c4ea569aea47261c1d91e11a
matias-ibserveis <matias@ibserveis.com>
2021-02-26 18:25:12 +0100

    list_1 OK

--- curso_next_part2/src/pages/list_1.js
+++ curso_next_part2/src/pages/list_1.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
 export default function List({usuariosList}) {
-  
-  return (
-    <div>
-      {usuariosList.map((element, index) => (
+
+  const crearLista = (lista) => {
+
+    const lista_formato = lista.map((element, index) => 
         <div key={index}>
           <Link as={`${element.name}`} href={`/usuario?id=${element.id}`}>
             <a>
@@ -13,15 +13,20 @@ export default function List({usuariosList}) {
             </a>
           </Link>
         </div>
-      ))}
+    )
+    return lista_formato
+  }
+  
+  return (
+    <div>
+      { crearLista(usuariosList) }
     </div>
   );
 }
 
 List.getInitialProps = async () => {
-  //const response = await fetch('http://localhost:4001/vehicles');
   //const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const response = await fetch('http://localhost:4001/usuarios')
-  const usuariosList = await response.json();
-  return {usuariosList: usuariosList}
+  const listadoDatos = await response.json();
+  return {usuariosList: listadoDatos}
 }
